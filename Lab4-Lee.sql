8e1e55f182db7bcfdaeb77eebcfbd9132e48ef3f
briannaxlee <briannaxlee@gmail.com>
2020-09-24 17:09:40 -0400

    Brianna Lee - Lab 4 - 09.23.20
    the spacing looked strange in the first upload, so i uploaded it again but no other changes to the code besides that.

--- Lab4-Lee.sql
+++ Lab4-Lee.sql
@@ -3,69 +3,69 @@
 select *
 from People
 where pid in (select pid
-			 from Customers);
+			from Customers);
 			 
 select *
 from People
 where pid in (select pid
-			 from Agents);
+			from Agents);
 			 
 select *
 from People
 where pid in (select pid
-			 from Agents
-			 where pid in (select pid
-						   from Customers));
+			from Agents
+			where pid in (select pid
+						from Customers));
 
 select *
 from People
 where not pid in (select pid
-				  from Customers)
-				  and not pid in (select pid
-								   from Agents);
+				from Customers)
+				and not pid in (select pid
+								from Agents);
 								   
 select pid
 from Customers
 where pid in (select custId
-			  from Orders
-			  where prodId = 'p01' or prodId = 'p07');
+			from Orders
+			where prodId = 'p01' or prodId = 'p07');
 			  
 select pid
 from Customers
 where pid in (select custId
-			  from Orders
-			  where prodId = 'p01'
-			  and custId in (select custId
-							from Orders
-							where prodId = 'p07'))
+			from Orders
+			where prodId = 'p01'
+			and custId in (select custId
+						from Orders
+						where prodId = 'p07'))
 order by pid desc;
 
 select firstName, lastName
 from People
 where pid in (select pid
-			 from Agents
-			 where pid in (select AgentId
-							 from Orders
-							 where prodId = 'p05' or prodId = 'p07'))
+			from Agents
+			where pid in (select AgentId
+						from Orders
+						where prodId = 'p05' or prodId = 'p07'))
 order by lastName desc;
 
 select homecity, DOB
 from People
 where pid in (select AgentId
-			 from Orders
-			 where custId = '001')
+			from Orders
+			where custId = '001')
 order by homecity asc;
 
 select prodId
 from Orders
 where agentId in (select agentId
-				 from Orders
-				 where quantityOrdered >= 1
-				 and custId in (select pid
-							   from People
-							   where homecity = 'Toronto'
-							   and pid in (select pid
-										  from Customers)))
+				from Orders
+				where quantityOrdered >= 1
+				and custId in (select pid
+							from People
+							where homecity = 'Toronto'
+							and pid in (select pid
+										from Customers)))
 order by prodId desc;
 
 select lastName, homecity
